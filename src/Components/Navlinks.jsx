f4c51c258074455bff1410d6c7b21fba648a02da
ashithareez <reez.as@northeastern.edu>
2024-04-07 15:05:24 -0400

    changed destination link

--- src/Components/Navlinks.jsx
+++ src/Components/Navlinks.jsx
@@ -7,8 +7,7 @@ const Navlinks = () => {
       <div className="hidden md:flex md:w-auto md:order-1">
        
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
-            <li>
-            <Link to="/destination" className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-900 md:p-0">Destinations</Link>
+            <li className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-900 md:p-0">Destinations
             </li>
             <li>
             <Link to="/deals" className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-900 md:p-0">Deals</Link>
