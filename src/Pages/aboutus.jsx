270693041fbcece43d9aea527c2a2364f54bfd7c
Ashwinipuranik30 <144976846+Ashwinipuranik30@users.noreply.github.com>
2024-04-17 16:56:45 -0400

    text content in about us pages

--- src/Pages/aboutus.jsx
+++ src/Pages/aboutus.jsx
@@ -1,6 +1,36 @@
 const About = () => {
     return (
-      <div>About page</div>
+      <div>
+
+        <section className="bg-white">
+              <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
+                <div className="bg-teal-500 border border-gray-200 rounded-lg p-8 md:p-12 mb-8">
+                  <h1 className="text-white text-3xl md:text-5xl font-extrabold mb-2">Who we are</h1>
+                  <p className="text-lg font-normal text-white mb-6 ">At Tripinn, our journey began with a singular goal: to enrich lives by making once-in-a-lifetime trips a reality for our customers. With a firm belief that international travel should be accessible to all, we set out to curate unforgettable experiences that transcend boundaries and create lasting memories. From the moment you step into our world, expect nothing less than personalized service, unwavering dedication, and a commitment to helping you explore the wonders of our diverse planet.</p>
+                </div>
+                <div className="grid md:grid-cols-2 gap-8">
+                  <div className="bg-gray-800 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+                    <h2 className="text-white text-3xl font-extrabold mb-2">Mission</h2>
+                    <p className="text-lg font-normal text-gray-100 dark:text-gray-400 mb-4 py-4">Our mission at Tripinn is to inspire and empower individuals to explore the world by providing exceptional travel experiences tailored to their unique interests and preferences. We strive to create unforgettable memories, foster cultural understanding, and promote sustainable travel practices. With our expertise, personalized service, and commitment to excellence, we aim to be the trusted partner for all our clients' travel needs, ensuring every journey is filled with adventure, discovery, and joy.</p>
+                  </div>
+                  <div className="bg-gray-800 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12">
+                    <h2 className="text-white text-3xl font-extrabold mb-2">Vision</h2>
+                    <p className="ext-lg font-normal text-gray-100 dark:text-gray-400 mb-4 py-4">Our vision is to inspire and enable people to explore the world by providing exceptional travel experiences that exceed expectations. We aim to be the preferred choice for travelers seeking personalized, immersive, and unforgettable journeys. Through innovation, integrity, and a commitment to excellence, we strive to create lifelong memories and foster a deeper appreciation for the diverse cultures and natural wonders of our planet.</p>
+                  </div>
+                </div>
+              </div>
+            </section>
+
+
+
+
+
+
+
+
+
+
+      </div>
     );
   };
   
