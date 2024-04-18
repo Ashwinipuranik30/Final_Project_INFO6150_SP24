adc1a3a18cae06a1b9119c77fb81c9e2652dca03
ashithareez <reez.as@northeastern.edu>
2024-04-17 23:54:50 -0400

    added text component

--- src/Pages/deals.jsx
+++ src/Pages/deals.jsx
@@ -1,9 +1,30 @@
+import Textcontainer from "../Components/Textcontainer";
 import SummerSaleSection from "../Components/dealheroimage";
 const Deals = () => {
     return (
+      <>
       <div>
         <SummerSaleSection />
       </div>
+      <div>
+     
+      <Textcontainer
+        Title="Summer Flash Sale"
+        Description="Don't miss out on this unbeatable offer. Hurry, the clock is ticking — Shop now and save big!"
+        additionalContent="Reserve your tour before May 15th to lock in this special deal."
+      />
+      
+    </div>
+    <div>
+     
+      <Textcontainer
+        Title="Save on Dream Destinations"
+        Description="Discover irresistible deals you won't want to miss out on! View our travel deals below to find an affordable tour that fits your budget, available year-round."
+        
+      />
+      
+    </div>
+    </>
     );
   };
   
