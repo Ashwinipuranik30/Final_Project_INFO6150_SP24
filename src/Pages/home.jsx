668bf0a2d024c87b6bb404e0af79440d64fd7296
Ashwinipuranik30 <144976846+Ashwinipuranik30@users.noreply.github.com>
2024-04-18 21:44:03 -0400

    update

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -54,7 +54,7 @@ const Home = () => {
       <div>
         <Banner />
 
-        <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
+      <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
       <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 gap-10">
         {cards.map((card, index) => (
           <DealCard
